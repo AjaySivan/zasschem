a3b4a9d583bf4aaaf693997d0ecaf75f2650b15a
Satietyfrm <satietyfoodresearch@gmail.com>
2024-06-20 12:44:55 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/*-----------------     progressive counter script    -----------------*/
+
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
